b6e5ab5b8b0dade6f9b585fcaad97b49c7c8cef4
blue_wizzrobe <kevin@mangakissa.nl>
2022-01-26 12:16:00 +0100

    recepten aangepast

--- tabelScript.js
+++ tabelScript.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     fetch('https://yc2112-backend.azurewebsites.net/receptenlijst')
         .then(result => result.json())
-        .then(products => {
-            console.log(products);
-            toevoegenAanTabel(products);
+        .then(recepten => {
+            toevoegenAanTabel(recepten);
         });
 });
 
-function toevoegenAanTabel(dummyData) {
-    console.log(dummyData);
+function toevoegenAanTabel(recepten) {
     let data = document.getElementById("overzicht");
-    for(var x=0; x<dummyData.length; x++) {
+    for(var x=0; x<recepten.length; x++) {
         data.innerHTML+=`<div class="row">
-            <div class="col-sm-3" style="font-weight: 600;">${dummyData[x].bereidingstijd}</div>
-            <div class="col-sm-3" style="font-weight: 600;">${dummyData[x].naam}</div>
-            <div class="col-sm-6" style="font-weight: 600;">${dummyData[x].beschrijving}</div>
+            <div class="col-sm-3" style="font-weight: 600;">${recepten[x].bereidingstijd}</div>
+            <div class="col-sm-3" style="font-weight: 600;">${recepten[x].naam}</div>
+            <div class="col-sm-6" style="font-weight: 600;">${recepten[x].beschrijving}</div>
+            <div class="col-sm-6" style="font-weight: 600;">${recepten[x].bereiding}</div>
+            <div class="col-sm-6" style="font-weight: 600;">${recepten[x].afbeelding}</div>
         </div>`;
     }
 }
